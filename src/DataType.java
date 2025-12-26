b4808c159414396c5d19e71fc9960c7fe2c98104
Vamshi Batuka <vamshi.batuka@gmail.com>
2025-12-26 21:09:53 +0530

    Completed the variable and data types concepts

---
+++ src/DataType.java
@@ -0,0 +1,29 @@
+class DataType {
+    public static void main(String[] args){
+        double age = 25.55;
+        long population = 1400000000L;
+        float salary = 1255.5f;
+        double interest = 2.25;
+        char grade = 'A';
+        boolean isEmployed = true;
+        String name = "Vamshi";
+
+
+        System.out.println(age);
+        System.out.println(population);
+        System.out.println(salary);
+        System.out.println(interest);
+        System.out.println(grade);
+        System.out.println(isEmployed);
+        System.out.println(name);
+
+        double price = 99.99;
+        char grades = 'A';
+        boolean active = true;
+
+        System.out.println(price);
+        System.out.println(grades);
+        System.out.println(active);
+
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,16 @@
+class Variable {
+    public static void main(String[] args){
+        int age = 25;
+        System.out.println(age);
+
+        String city = "Hyderabad";
+        String country = "India";
+        int pincode = 500112;
+        boolean isMarried = false;
+
+        System.out.println(city);
+        System.out.println(country);
+        System.out.println(pincode);
+        System.out.println(isMarried);
+    }
+}
