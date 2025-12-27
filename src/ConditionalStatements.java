e3a171abe244a43fc9499128631f23a9a81f3079
Vamshi Batuka <vamshi.batuka@gmail.com>
2025-12-27 21:58:50 +0530

    Started the Conidtional Statements Concept

---
+++ src/ConditionalStatements.java
@@ -0,0 +1,12 @@
+class ConditionalStatements {
+    static void main(String[] args){
+    int a =-5;
+
+    if (a >= 0) {
+        System.out.println(" The "+a+" is postive value");
+    }
+    else {
+        System.out.println("The " + a + " is postive value");
+    }
+    }
+}
