fab952c58c98b8e880e3c7dc2e578db2aae0e29b
Vamshi Batuka <vamshi.batuka@gmail.com>
2025-12-29 12:41:25 +0530

    Completed the Conditinal Statement concept till if else topic

--- src/ConditionalStatements.java
+++ src/ConditionalStatements.java
@@ -1,12 +1,25 @@
 class ConditionalStatements {
-    static void main(String[] args){
-    int a =-5;
+    static void main(String[] args) {
+        int a = -5;
 
-    if (a >= 0) {
-        System.out.println(" The "+a+" is postive value");
-    }
-    else {
-        System.out.println("The " + a + " is postive value");
-    }
+        if (a >= 0) {
+            System.out.println(" The " + a + " is postive value");
+        } else {
+            System.out.println("The " + a + " is postive value");
+        }
+
+        int marks = 40;
+        if (marks >= 35) {
+            System.out.println("Pass");
+        }
+
+        int age = 16;
+        if(age>= 18){
+            System.out.println("Can Vote");
+        } else {
+            System.out.println("Cannot Vote");
+        }
+
+
     }
 }
